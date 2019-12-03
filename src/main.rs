5c0613dbfa96714028c538734f7afa1755f05e42
Ryan Jacobs <maroni720@gmail.com>
2019-12-02 22:27:06 -0600

    added mb config structures

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
 #![no_std]
 #![no_main]
 
-
-mod u8250;
 mod machine;
+mod u8250;
+mod config;
 
+use core::fmt::Write;
 use core::panic::PanicInfo;
-use core::fmt::Write;  
 
 use u8250::U8250;
-
+use config::mb_info;
 
 static HELLO: &[u8] = b"Off to the races!\n";
 
 pub fn main() {}
 
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
-    let mut uart = U8250{};
+pub extern "C" fn _start(mb_config: &mb_info) -> ! {
+    let mut uart = U8250 {};
     let hi = "Hello there!\n";
     uart.write_string(hi);
-    write!(uart, "The numbers are {} and {}, {}\n", 42, 1.0/3.0, hi).unwrap();
-    println!("ooooweee, we're using println, {} {} {}", 42, 1.0/3.0, hi);
+    write!(uart, "The numbers are {} and {}, {}\n", 42, 1.0 / 3.0, hi).unwrap();
+    println!("ooooweee, we're using println, {} {} {}", 42, 1.0 / 3.0, hi);
+    mb_config.print();
+    //println!("mb config at 0x{:x}", mb_config as *const u32);
     for (i, &byte) in HELLO.iter().enumerate() {
         uart.put(byte as u8);
     }
