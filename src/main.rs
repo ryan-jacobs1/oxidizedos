f4c8d80095896d78db8648e8e5c39c83afbbd50d
Ryan Jacobs <rjacobs@cs.utexas.edu>
2019-12-10 00:36:12 -0600

    stuff for heap

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
+
 
 mod machine;
 mod u8250;
 mod config;
+mod heap;
+
+extern crate alloc;
+
+use alloc::{boxed::Box, vec, vec::Vec};
 
 use core::fmt::Write;
 use core::panic::PanicInfo;
 
 use u8250::U8250;
 use config::mb_info;
+use heap::{Heap, LockedHeap, Block};
+
 
 static HELLO: &[u8] = b"Off to the races!\n";
+
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::new();
 
 pub fn main() {}
 
@@ -27,10 +40,17 @@ pub extern "C" fn _start(mb_config: &mb_info) -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         uart.put(byte as u8);
     }
+    let heap_val = Box::new(41);
+    println!("value on heap {}", heap_val);
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
+}
+
+#[alloc_error_handler]
+fn alloc_panic(layout: alloc::alloc::Layout) -> ! {
+    panic!("Failure in alloc");
 }
