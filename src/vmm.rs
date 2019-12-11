bf550e7bfd0e3925e46e6e204996d866acd2641c
Ryan Jacobs <rjacobs@cs.utexas.edu>
2019-12-10 22:02:55 -0600

    implemented vpn to ppn mapping function

--- src/vmm.rs
+++ src/vmm.rs
@@ -1,14 +1,11 @@
 extern crate spin;
 
-
-
 use spin::Mutex;
 
-
-use crate::println;
-use crate::config::mb_memory_map;
 use crate::config::mb_info_memory;
 use crate::config::mb_info_memory_entry;
+use crate::config::mb_memory_map;
+use crate::println;
 
 /*
  * Not a particularly impressive allocator, but works fine in QEMU.
@@ -22,23 +19,79 @@ pub struct VMAllocator {
     end_phys_mem: u64,
 }
 
-#[repr(C, packed)]
+#[repr(C, align(4096))]
 pub struct AddressSpace {
     entries: [AddressSpaceEntry; 512],
 }
 
+impl AddressSpace {
+    pub fn new() -> *mut AddressSpace {
+        alloc() as *mut AddressSpace
+    }
+    pub fn create_mapping(&mut self, vpn: u64, ppn: u64) {
+        self.create_mapping_helper(Address{0: vpn}, ppn, 4);
+    }
+    fn create_mapping_helper(&mut self, vpn: Address, ppn: u64, level: u32) {
+        let index = match level {
+            4 => vpn.pml4_index(),
+            3 => vpn.pdpt_index(),
+            2 => vpn.pd_index(),
+            1 => vpn.pt_index(),
+            _ => {panic!("Invalid paging structure level");}
+        } as usize;
+        let mut entry = &mut self.entries[index];
+        match level {
+            1 => {
+                entry.set_present(1);
+                entry.set_physical_addr(ppn);
+            }
+            2..=4 => {
+                if (entry.present() == 0) {
+                    entry.set_present(1);
+                    entry.set_physical_addr(alloc());
+                }
+                entry.get_address_space().create_mapping_helper(vpn, ppn, level - 1);
+            }
+            _ => {panic!("Invalid paging structure level");}
+        }
+    }
+}
+
 bitfield! {
-    #[repr(C, packed)]
+    #[repr(transparent)]
+    pub struct Address(u64);
+    u64;
+    pt_index, _: 8, 0;
+    pd_index, _: 17, 9;
+    pdpt_index, _: 26, 18;
+    pml4_index, _: 35, 27;
+}
+
+bitfield! {
+    #[repr(transparent)]
     pub struct AddressSpaceEntry(u64);
     present, set_present: 0, 0;
     writable, set_writable: 1, 1;
     user_supervisor, set_user_supervisor: 2, 2;
     u64;
     physical_addr, set_physical_addr: 51, 12;
-}
-static VMM_ALLOCATOR: Mutex<VMAllocator> = spin::Mutex::new(VMAllocator{next: 0, start_phys_mem: 0x150000, end_phys_mem: 0});
-static PAGE_SIZE: u64 = 0x1000;
 
+}
+
+impl AddressSpaceEntry {
+    pub fn get_address_space(&self) -> &mut AddressSpace {
+        unsafe {
+            &mut *((self.physical_addr() + PAGE_SIZE) as *mut AddressSpace)
+        }
+    }
+}
+
+static VMM_ALLOCATOR: Mutex<VMAllocator> = spin::Mutex::new(VMAllocator {
+    next: 0,
+    start_phys_mem: 0x150000,
+    end_phys_mem: 0,
+});
+static PAGE_SIZE: u64 = 0x1000;
 
 pub fn init() {
     let mut vmm_allocator = VMM_ALLOCATOR.lock();
@@ -59,30 +112,35 @@ pub fn init() {
         }
     }
     println!("end_phys_mem: {:x}", vmm_allocator.end_phys_mem);
-    let x = AddressSpaceEntry{0: 1};
+    let x = AddressSpaceEntry { 0: 1 };
     let y = core::mem::size_of::<AddressSpaceEntry>();
     println!("sizeof entry is {}", y);
-    println!("sizeof address space is 0x{:x}", core::mem::size_of::<AddressSpace>());
+    println!(
+        "sizeof address space is 0x{:x}",
+        core::mem::size_of::<AddressSpace>()
+    );
     x.present();
 }
 
 pub fn alloc() -> u64 {
     let mut vmm_allocator = VMM_ALLOCATOR.lock();
+    let result: u64;
     if (vmm_allocator.start_phys_mem != vmm_allocator.end_phys_mem) {
-        let result = vmm_allocator.start_phys_mem;
+        result = vmm_allocator.start_phys_mem;
         vmm_allocator.start_phys_mem += PAGE_SIZE;
-        result
-    }
-    else {
+    } else {
         // TODO: Demand paging
         if (vmm_allocator.next == 0) {
             panic!("Out of physical frames.");
         }
-        let result = vmm_allocator.next;
+        result = vmm_allocator.next;
         let ptr = result as *mut u64;
         unsafe {
             vmm_allocator.next = *ptr;
         }
-        result
     }
+    unsafe {
+        core::ptr::write_bytes(result as *mut u8, 0, PAGE_SIZE as usize);
+    }
+    result
 }
